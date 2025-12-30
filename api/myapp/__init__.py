8f3597715d19c56b86200a3033547eaa76a970b4
Peter Pimley <peter.pimley@gmail.com>
2025-12-30 22:14:35 +0800

    Used werkzeug to tell Flask that it is behind a proxy.

--- api/myapp/__init__.py
+++ api/myapp/__init__.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import (Flask, request)
+
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 def create_app():
     a = Flask (__name__)
 
+
+    a.wsgi_app = ProxyFix(
+        a.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    )
+
     @a.get("/api/")
     def hello():
-        return "Hello from Flask"
+        return f"Hello from Flask.  You are {request.remote_addr}."
     return a
