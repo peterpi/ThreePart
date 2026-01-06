b6043ab3b3ddc9972d301c3691e90e39ab7fe49e
Peter Pimley <peter.pimley@gmail.com>
2026-01-06 20:58:35 +0800

    Work in progress for database updater.

--- db_updater/upgrade.py
+++ db_updater/upgrade.py
@@ -1,2 +1,36 @@
+import psycopg
+import re
+import os
 
-print ("Hhello world.")
+a_b_form = re.compile(r"(\d+)_(\d+)\.sql")
+
+topVersion = 0
+
+for dirent in os.scandir ("upgrades"):
+	if not dirent.is_file():
+		continue
+	name = dirent.name
+	print (f"Considering {name}")
+	match = a_b_form.fullmatch(name)
+	assert (match)
+	fromVersion = int(match.group(1))
+	toVersion = int(match.group(2))
+	topVersion = max (topVersion, toVersion)
+	print (f"{name} goes from {fromVersion} to {toVersion}.")
+
+
+
+print ("Hello world.")
+
+db = psycopg.connect ("postgres://postgres@db/bookings-orgs", password = "Hello")
+
+version = 0
+try:
+	version = int(db.execute("SELECT version FROM globals").fetchone()[0])
+except Exception as x:
+	pass # version stays at zero.
+
+
+print (f"Database at version {version}, scripts up to version {topVersion}.")
+
+
