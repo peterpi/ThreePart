fc7c1647a3d21efe9fa217d3dcbf70842c521e87
Peter Pimley <peter.pimley@gmail.com>
2026-01-11 04:43:09 +0000

    Fixed some newbie import errors.

--- api/myapp/auth.py
+++ api/myapp/auth.py
@@ -1,4 +1,4 @@
-
+from flask import (session,)
 from flask_httpauth import (HTTPBasicAuth)
 from . import orgindex
 
@@ -7,9 +7,14 @@ auth = HTTPBasicAuth()
 
 @auth.verify_password
 def _verify(username, password):
-	return {
-		"org" : "whatever"
-	}
-
-
-here set up database for orgs etc
+	# If there is already a session then allow them through.
+	u = session.get("user")
+	if u:
+		return u
+	with orgindex.get_db() as index:
+		cur = orgindex.execute ("SELECT * FROM org_user WHERE id = %s", (username))
+		row = cur.fetchone()
+		if not row:
+			return False
+		u = dict (cur.fetchone())
+		return u
