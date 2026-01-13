24bba608d52da39225090879c37119cac831e601
Peter Pimley <peter.pimley@gmail.com>
2026-01-13 11:50:45 +0000

    Use dict_row for orgindex.py

--- api/myapp/orgindex.py
+++ api/myapp/orgindex.py
@@ -1,7 +1,9 @@
 import psycopg
+import psycopg.rows
 
 
 def get_db ():
 	url = f"postgres://bookings@db/bookings-orgs"
 	db = psycopg.connect (url, password = "Hello")
+	db.row_factory = psycopg.rows.dict_row
 	return db
