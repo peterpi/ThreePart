ac09605f9a500cabfd11a0d5890fded5baf0671e
Peter Pimley <peter.pimley@gmail.com>
2026-02-23 08:48:10 +0000

    More work on service editing.

--- api/myapp/services.py
+++ api/myapp/services.py
@@ -15,12 +15,27 @@ def get_all():
 	return {"services" : all}
 
 
-@bp.post("/")
+@bp.post("")
 @auth.login_required
 def post_new():
 	j = request.json
 	name = j["name"] or abort (400)
 	with get_db() as db:
-		db.execute ("INSERT INTO service (name) VALUES (%s)", (name,))
-	return ("", 204)
+		try:
+			cur = db.execute ("INSERT INTO service (name, uuid) VALUES (%s, uuidv4()) RETURNING name, uuid", (name,))
+			row = cur.fetchone()
+			return row
+		except:
+			abort (400) # Blame the user :p
 
+@bp.put("<uuid:uuid>")
+@auth.login_required
+def put_existing(uuid):
+	j = request.json
+	name = j["name"] or abort (400)
+	with get_db() as db:
+		cur = db.execute ("UPDATE service SET name = %s WHERE uuid = %s RETURNING name,uuid", (name, uuid))
+		row = cur.fetchone()
+		if not row:
+			abort (404)
+		return row
--- frontend/content/services.js
+++ frontend/content/services.js
@@ -5,32 +5,86 @@ var templates = await fetch ("./services.html")
 
 class Service
 {
+	uuid
 	name
 	duration
 
 	constructor (j)
 	{
+		this.uuid = j.uuid
 		this.name = j.name
 		this.duration = j.duration
 	}
 }
 
-export class ServicesEditor
+class ServiceEvent extends Event 
+{
+	s
+
+	constructor (type, s, opts) {
+		super(type, opts)
+		this.s = s
+	}
+}
+
+class ServiceEditEvent extends ServiceEvent
+{
+	details
+	constructor (type, s, details, opts) {
+		super (type, s, opts)
+		this.details = details
+	}
+}
+
+export class ServicesEditor extends EventTarget
 {
 	view
 
 	constructor()
 	{
-		this.view = document.createElement("bookings-serviceseditor")
-		this.view.editor = this
+		super()
+		var view = document.createElement("bookings-serviceseditor")
+		view.editor = this
+		view.addEventListener("edit-requested", e => this.put(e.s, e.details))
+		this.view = view
 	}
 
-	getAll () // Returns a promise to an array of Service objects.
+	// Emits "got-service" for all services.
+	async getAll ()
 	{
-		var servicesPromise = fetch ("/api/services")
+		var response = await fetch ("/api/services")
 			.then (r => r.json())
 			.then (j => j.services.map (s => new Service(s)))
-		return servicesPromise
+		for (var s of response) {
+			var e = new CustomEvent ("got-service", {detail: s})
+			this.dispatchEvent(e)
+		}
+	}
+
+	// Alter an existing service.
+	put (s, details)
+	{
+		var j = JSON.stringify(details)
+		var url = `/api/services/${s.uuid}`
+		var p = fetch (url, {
+			method: "PUT",
+			headers: {"Content-Type": "application/json"},
+			body: j
+		})
+	}
+
+	async createService (details)
+	{
+		var url = `/api/services`
+		var s = await fetch (url, {
+			method: "POST",
+			headers: {"Content-Type" : "application/json"},
+			body: JSON.stringify(details)
+		})
+		.then (resp => resp.json())
+		.then (j => new Service (j))
+		this.dispatchEvent(new CustomEvent ("got-service", {detail:s}))
+		return s
 	}
 }
 
@@ -50,7 +104,16 @@ class ServiceEditorView extends HTMLElement
 	connectedCallback() {
 		var shadow = this.attachShadow({mode:"closed"})
 		shadow.appendChild (serviceEditorTemplate.content.cloneNode(true))
-		shadow.querySelector("#name").value = this.s.name
+		var nameInput = shadow.querySelector("#name")
+		nameInput.value = this.s.name
+		nameInput.addEventListener("change", _ => {
+			this.dispatchEvent(
+				new ServiceEditEvent (
+					"edit-requested",
+					this.s,
+					{"name": nameInput.value},
+					{bubbles: true, composed:true}), )
+		})
 	}
 
 }
@@ -58,17 +121,56 @@ class ServiceEditorView extends HTMLElement
 customElements.define("bookings-serviceeditor", ServiceEditorView)
 
 
+class ServicesEditorNewServiceForm extends HTMLElement
+{
+	constructor()
+	{
+		super()
+	}
+
+	#name
+
+	connectedCallback()
+	{
+		var template = templates.querySelector("#services-editor-new-service")
+		var shadow = this.attachShadow({mode:"closed"})
+		var clone = template.content.cloneNode(true)
+		shadow.appendChild(clone)
+		this.#name = shadow.querySelector("#name")
+		var button = shadow.querySelector("button")
+		button.addEventListener("click", e => this.#requestCreate())
+	}
+
+	clear()
+	{
+		this.#name.value = ""
+	}
+
+	#requestCreate()
+	{
+		var details = {
+			name: this.#name.value
+		}
+		var e = new CustomEvent ("create-requested", {detail:details})
+		this.dispatchEvent(e)
+	}
+}
+
+customElements.define("bookings-serviceseditor-new-service", ServicesEditorNewServiceForm)
+
 // Many services
 class ServicesEditorView extends HTMLElement
 {
 	editor
+	#shadow
+	#create
 
 	constructor ()
 	{
 		super()
 	}
 
-	connectedCallback() {
+	async connectedCallback() {
 		var template = templates.querySelector("#services-editor")
 		var shadow = this.attachShadow({mode:"closed"})
 		shadow.appendChild(template.content.cloneNode(true))
@@ -76,17 +178,38 @@ class ServicesEditorView extends HTMLElement
 		shadow.querySelector("#back").addEventListener("click", _ => {
 			this.dispatchEvent(new Event ("back-requested"))
 		})
+		this.#shadow = shadow
 
-		var rows = shadow.querySelector("#rows")
-		this.editor.getAll()
-			.then (services => {
-				services.forEach(s => {
-					var sv = document.createElement("bookings-serviceeditor")
-					sv.s = s
-					rows.appendChild(sv)
-				});
-			})
+		var create = shadow.querySelector("#create")
+		create.addEventListener("create-requested", e => this.#createService(e.detail))
+		this.#create = create
 
+		this.editor.addEventListener("got-service", e => this.showService(e.detail))
+		this.editor.getAll() // Will emit zero-or-more events of type "got-service".
+		shadow.getElementById("loading").remove()
+	}
+
+
+	showService (details)
+	{
+		console.assert(details)
+		var sv = document.createElement("bookings-serviceeditor")
+		sv.s = details
+		var rows = this.#shadow.querySelector("#rows")
+		rows.appendChild(sv)
+	}
+
+	async #createService (details)
+	{
+		var create = this.#create
+		create.setAttribute ("inert", true)
+		try{
+			await this.editor.createService (details) // Could fail, e.g. dupliate name
+			create.clear()
+		}
+		finally {
+			create.removeAttribute("inert")
+		}
 	}
 
 
