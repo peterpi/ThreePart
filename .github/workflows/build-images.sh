78d66590e6281b9f25e24ef5483fa73dbe97e736
Peter Pimley <peter.pimley@gmail.com>
2026-07-24 12:53:51 +0800

    Build DB image as well.

--- .github/workflows/build-images.sh
+++ .github/workflows/build-images.sh
@@ -1,10 +1,12 @@
 set -e
 
+docker build -t peterpimley/bookings-db:$GITHUB_REF_NAME db
 docker build -t peterpimley/bookings-api:$GITHUB_REF_NAME api
 docker build -t peterpimley/bookings-frontend:$GITHUB_REF_NAME frontend
 
 
 echo $DOCKER_HUB_PAT | docker login --password-stdin -u peterpimley
 
+docker push peterpimley/bookings-db:$GITHUB_REF_NAME
 docker push peterpimley/bookings-api:$GITHUB_REF_NAME
 docker push peterpimley/bookings-frontend:$GITHUB_REF_NAME
