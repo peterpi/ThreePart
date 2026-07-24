bebf9978e825b0fc85c3fc136cf502ae03a776f3
Peter Pimley <peter.pimley@gmail.com>
2026-07-24 11:00:53 +0800

    Build frontend too.

--- .github/workflows/build-images.sh
+++ .github/workflows/build-images.sh
@@ -1,8 +1,10 @@
-
+set -e
 
 docker build -t peterpimley/bookings-api:latest api
+docker build -t peterpimley/bookings-frontend:latest frontend
 
 
 echo $DOCKER_HUB_PAT | docker login --password-stdin -u peterpimley
 
 docker push peterpimley/bookings-api:latest
+docker push peterpimley/bookings-frontend:latest
