f084e86cd62f17f5ef065d3740882f18a21a61ca
Peter Pimley <peter.pimley@gmail.com>
2026-07-24 11:11:13 +0800

    Auto build images (#2)
    * Build images on push.
    
    * u+x on build-images.sh
    
    * Build frontend too.
    
    * Use branch name for Docker image tag.

---
+++ .github/workflows/build-images.sh
@@ -0,0 +1,10 @@
+set -e
+
+docker build -t peterpimley/bookings-api:$GITHUB_REF_NAME api
+docker build -t peterpimley/bookings-frontend:$GITHUB_REF_NAME frontend
+
+
+echo $DOCKER_HUB_PAT | docker login --password-stdin -u peterpimley
+
+docker push peterpimley/bookings-api:$GITHUB_REF_NAME
+docker push peterpimley/bookings-frontend:$GITHUB_REF_NAME
