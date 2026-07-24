633a96a79d25383995cf657ffaf32e8b27442df8
Peter Pimley <peter.pimley@gmail.com>
2026-07-24 11:05:09 +0800

    Use branch name for Docker image tag.

--- .github/workflows/build-images.sh
+++ .github/workflows/build-images.sh
@@ -1,10 +1,10 @@
 set -e
 
-docker build -t peterpimley/bookings-api:latest api
-docker build -t peterpimley/bookings-frontend:latest frontend
+docker build -t peterpimley/bookings-api:$GITHUB_REF_NAME api
+docker build -t peterpimley/bookings-frontend:$GITHUB_REF_NAME frontend
 
 
 echo $DOCKER_HUB_PAT | docker login --password-stdin -u peterpimley
 
-docker push peterpimley/bookings-api:latest
-docker push peterpimley/bookings-frontend:latest
+docker push peterpimley/bookings-api:$GITHUB_REF_NAME
+docker push peterpimley/bookings-frontend:$GITHUB_REF_NAME
