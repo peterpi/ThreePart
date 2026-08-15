cc7730c00890d7e7b7bcd78bcea877b8cad57c24
Peter Pimley <peter.pimley@gmail.com>
2026-08-15 16:19:45 +0800

    Auth gets user and their org in a single SQL transaction.

--- api/myapp/auth.py
+++ api/myapp/auth.py
@@ -11,15 +11,11 @@ def _verify(username, password):
 	u = session.get("user")
 	if u:
 		return u
-	with orgindex.get_db() as index:
-		cur = index.execute ("SELECT * FROM org_user WHERE id = %s", (username,))
+	with orgindex.get_db() as db:
+		cur = db.execute ("SELECT org.id AS org, account.email AS email FROM org JOIN org_account_membership ON org.id = org_account_membership.org JOIN account on account.id = org_account_membership.account WHERE account.email = %s", (username,))
 		user = cur.fetchone()
 		if not user:
 			return False
-		orgId = str(user["org"])
-		cur = index.execute ("SELECT * FROM org WHERE id = %s", (orgId,))
-		org = cur.fetchone()
 		u = dict (user)
-		u["org"] = org
 		session["user"] = u
 		return u
