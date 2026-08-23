a62251069250b73f1185bdae4153808e66bead1c
Peter Pimley <peter.pimley@gmail.com>
2026-08-23 15:36:26 +0800

    Starting to think about tenant index db, with the tenant table also holding the hostname of its database server.

---
+++ tenant_index/docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE account (
+	id uuid primary key default gen_random_uuid(),
+	email text unique not null
+);
+
+CREATE TABLE installation (
+	id uuid primary key default gen_random_uuid(),
+	superuser UUID NOT NULL REFERENCES account(id)
+);
+
+
+CREATE TABLE dbHost (
+	id SERIAL PRIMARY KEY,
+	hostname TEXT NOT NULL UNIQUE
+);
+
+INSERT INTO dbHost (hostname) VALUES ('db');
+
+
+CREATE TABLE org (
+	id UUID PRIMARY KEY default gen_random_uuid(),
+	orgname TEXT NOT NULL UNIQUE,
+	dbHost INTEGER NOT NULL REFERENCES dbHost(id) ON DELETE RESTRICT,
+	dbName TEXT NOT NULL
+);
+
+CREATE TABLE org_account_membership (
+	org UUID NOT NULL REFERENCES org(id),
+	account UUID NOT NULL REFERENCES account(id)
+);
